a3157a1aa35caa9868d03e113c5e910bc3719cbc
Zach Berkowitz <zachb1217@gmail.com>
2017-02-21 14:41:36 +0100

    Initial Commit

---
+++ GraphicsPrac2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GraphicsPrac2
+//
+//  Created by Zachary Berkowitz on 2/21/17.
+//  Copyright © 2017 Zach Berkowitz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
